f6788249a2529c9b47ffda33db7746a6a51b9ba9
André Frederico Lucas da Silva <andre.frederico@ifpr.edu.br>
2021-03-11 15:59:56 -0300

    first commit

--- modules.tf
+++
@@ -1,5 +0,0 @@
-##############################################################################################
-### MODULOS GERAIS
-module "data_vpc_prod" {
-  source = "git::https://github.com/fredsilvas/aws_modules_data_vpc_prod.git"
-}
